dddfbee7bc3200fcf898540b71c57d9e4c844048
DELL <DELL@DESKTOP-LUK6UD5>
2021-07-21 10:27:14 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp3
+{
+    class Class1
+    {
+        public void file(string dir, int ndays)
+        {
+            //dir = @"C:\Users\DELL\AppData\Local\Temp";
+            //ndays = 3;
+            string[] Files = Directory.GetFiles(dir);
+            foreach (string f in Files)
+            {
+                if (File.Exists(f) && dir.Length > 5)
+                {
+                    FileInfo fi = new FileInfo(f);
+                    if (fi.LastAccessTime <= DateTime.Now.AddDays(ndays))
+                    {
+                        fi.Delete();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("file not exist or string length is less than 5");
+                }
+
+            }
+        }
+
+
+
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dir = @"C:\TestingDeleteFile";
+            int ndays = 1;
+            Class1 c = new Class1();
+            c.file(dir, ndays);
+            //string directory = @"C:\Users\DELL\AppData\Local\Temp";
+
+            //if (Directory.Exists(directory))
+            //{
+            //    //if() 
+            //    Console.WriteLine("exists");
+            //    System.Console.ReadLine();
+            //}
+        }
+    }
+}
